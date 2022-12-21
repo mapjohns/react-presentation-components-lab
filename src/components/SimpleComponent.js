5a7acbe4fff255f70cb34e0122802d6c35d92b1c
Matthew <mattj20@gmail.com>
2022-12-20 18:18:23 -0800

    commit tree e520a7d81581321ffb09be940f3bebbf26f0c276

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
 // Code SimpleComponent Here
+import React, { Component } from "react";
+
+export default class SimpleComponent extends Component {
+    constructor() {
+        super()
+        this.state={
+            mood:"happy"
+        }
+    }
+
+    toggleMood = () => {
+        let newState
+        this.state.mood === "happy" ? newState = "sad" : newState = "happy"
+        this.setState({
+            mood: newState
+        })
+    }
+
+    render() {
+        return(
+            <div onClick={this.toggleMood}>{this.state.mood}</div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={console.log("I am just sad")} />
   </div>,
   document.getElementById('root')
 );
